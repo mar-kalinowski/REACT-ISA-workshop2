0f327d6db4a98f423d8d7fb46a9d6c06ef2ec16d
Marcin Kalinowski <mjk.gdansk@gmail.com>
2022-03-28 09:41:49 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # REACT-ISA-workshop2
+
+1. Wykonanie aplikacji React, zawierającej podstrony:
+    - startowa
+    - o blogu
+    - posty  
+    - podstrona szczegóły postu
+    - dodaj post z formularzem
---
+++ project/src/components/Nav.js
@@ -0,0 +1,11 @@
+import {NavLink} from 'react-router-dom'
+
+export const Nav = () => {
+    return (
+        <div>
+            <ul>
+                <li></li>
+            </ul>
+        </div>
+    )
+}
