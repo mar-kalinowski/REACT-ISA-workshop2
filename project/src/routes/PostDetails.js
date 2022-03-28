026dbca5e4f2b41c89cb5823118e6826c90d10f3
Marcin Kalinowski <mjk.gdansk@gmail.com>
2022-03-28 12:29:53 +0200

    useState, useEffect added

--- project/src/routes/PostDetails.js
+++ project/src/routes/PostDetails.js
@@ -1,8 +1,12 @@
+import { useParams } from "react-router-dom";
+
 import { Header } from "../components/Header";
 import { Content } from "../components/Content";
 import { Footer } from "../components/Footer";
 
 export const PostDetails =() => {
+    const params = useParams();
+    console.log(params)
   return (
     <div>
      <Header />
--- project/src/routes/Posts.js
+++ project/src/routes/Posts.js
@@ -1,16 +1,27 @@
 import { Header } from "../components/Header";
 import { Content } from "../components/Content";
 import { Footer } from "../components/Footer";
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 export const Posts = () => {
-  return (
-    <div>
-     <Header />
-     Lista postów
-     <Content />
-     <Footer />
-    </div>
-  );
+    const [ posts, setPosts ] = useState([])
+
+    useEffect(() => {
+        fetch('https://localhost:3000/posts')
+        .then(r => r.json())
+        .then(posts => setPosts(posts));
+    }, []);
+
+    const renderPosts = () => posts.map(post => {
+        const postURL = `/post/${post.id}`;
+
+        return (
+            <div>
+                <Link to={postURL} key={post.id}>{post.title}</Link>
+            </div>
+        )
+    })
 }
 
 
